c41491316e5cc23b250fd05a1d53b5ce07dc9bf9
arspy <arspy@ARSEN-PC>
2015-11-10 15:23:00 +0400

    1. Minor path fix

--- admin/templates/topbar.php
+++ admin/templates/topbar.php
@@ -2,7 +2,7 @@
 	<div class="content-main">
 		<div class="topBar">
 			<ul class="nav-left">
-				<li><a href="<?=PATH ?>/admin/" class="nav-activ">Places</a></li>
+				<li><a href="<?=PATH ?>admin/" class="nav-activ">Places</a></li>
                                 <li><a href="?view=add_place" class="nav-activ">Add place</a></li>
 			</ul>
                     
