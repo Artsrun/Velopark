f16c4b8f8ba1ff5d79c3c6bbe0b81606cff0b7e6
arspy <arspy@ARSEN-PC>
2015-11-27 17:32:49 +0400

    1. Added logging 2. Fixed admin extension

--- admin/upload.php
+++ admin/upload.php
@@ -2,7 +2,7 @@
 
 $uploaddir = 'temp/';
 if (!empty($_FILES['change'])) {
-	$ext = pathinfo($_FILES["change"]["name"], PATHINFO_EXTENSION);
+	$ext = strtolower(pathinfo($_FILES["change"]["name"], PATHINFO_EXTENSION));
     if (move_uploaded_file($_FILES['change']['tmp_name'], $uploaddir . "tmp.".$ext)) {
         echo $uploaddir . "tmp.".$ext;
     } else {
